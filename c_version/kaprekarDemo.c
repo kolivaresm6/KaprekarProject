345c3a7bda8b1569edbff1068c16aebcc19d83e1
kolivaresm6 <35849002+kolivaresm6@users.noreply.github.com>
2018-01-26 14:38:02 -0600

    Update kaprekarDemo.c

--- c_version/kaprekarDemo.c
+++ c_version/kaprekarDemo.c
@@ -1,18 +1,21 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-#include "kaprekarUtils.h"
+#include<kaprekarUtils.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-  if(argc != 2) {
-    fprintf(stderr, "Usage: %s n\n", argv[0]);
+  if(argc !=2)
+  {
+    printf(stderr, "Usage: %d \n", argv[0]);
     exit(1);
   }
 
   int n = atoi(argv[1]);
 
-  if(isKaprekar(n)) {
+  if(isKaprekar(n))
+  {
     printf("%d is a Kaprekar Number!\n", n);
   } else {
     printf("%d is not a Kaprekar Number!\n", n);
